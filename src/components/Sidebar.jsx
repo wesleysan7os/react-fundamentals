45bbdca28b82f3d944488999d3e7dd24495f01ed
wesleysan7os <wesley.santos.comp@gmail.com>
2023-11-23 19:04:38 -0300

    feat: sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,22 @@
+import { PencilSimpleLine } from 'phosphor-react'
 import styles from './Sidebar.module.css'
 
 export const Sidebar = () => {
   return (
-    <aside className={styles.aside}>
-			Sidebar
+    <aside className={styles.sidebar}>
+			<img className={styles.cover} src="https://images.unsplash.com/photo-1516259762381-22954d7d3ad2?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+			<div className={styles.profile}>
+				<img className={styles.avatar} src="https://avatars.githubusercontent.com/u/12885110?v=4" />
+				<strong>Wesley Santos</strong>
+				<span>Dev Front-end</span>
+			</div>
+
+			<footer>
+				<a href="#">
+					<PencilSimpleLine size={20} weight="bold" />
+          Editar seu perfil
+        </a>
+			</footer>
     </aside>
   )
 }
